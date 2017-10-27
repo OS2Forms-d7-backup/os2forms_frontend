e77296309fd5346bd98f7fe73447147d6089cf52
juuliabellcom <juulia@bellcom.dk>
2017-10-27 12:27:18 +0300

    Added date_popup_component module

---
+++ modules/bellcom_webform_date_popup/js/init.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+(function ($) { 
+    $('.datepicker-wrap .input-group.date').datepicker({
+        language: "da",
+        autoclose: true,
+        format: 'dd/mm/yyyy',
+        todayHighlight: true,
+    });
+
+}(jQuery));
