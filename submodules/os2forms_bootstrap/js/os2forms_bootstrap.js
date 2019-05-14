a7fdd54cf1e1fdd0da0a596cfc3e4d7c7accbfc8
juuliabellcom <juuliadevadze@gmail.com>
2019-05-14 10:47:11 +0300

    Autocomplete issue

---
+++ submodules/os2forms_bootstrap/js/os2forms_bootstrap.js
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+jQuery(document).ajaxComplete(function(e) {
+  function dropdownWidth(){
+    var width = jQuery('.dropdown').parent('.form-item').find('.input-group').width();
+    var input_offset = jQuery('.dropdown').parent('.form-item').find('.input-group').offset();
+    var dropdown_offset = jQuery('.dropdown').offset();
+    jQuery('.dropdown').width(width);
+    jQuery('.dropdown').offset({top:dropdown_offset.top, left: input_offset.left});
+  }
+  dropdownWidth();
+  jQuery(window).resize(function(e) {
+    dropdownWidth();
+  });
+});
